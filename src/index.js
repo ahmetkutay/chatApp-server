53b68a2effb7c15ed6196bcc0ffaf1757eccd20a
Kutay Karacair <akkaracair@gmail.com>
2023-06-20 22:56:06 +0300

    quick fix for eslint error

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(helmet());
 
 if (process.env.NODE_ENV === 'development') {
-    app.use((err, req, res, next) => {
+    app.use((err, req, res) => {
         res.status(err.status || 500).json({
             error: {
                 message: err.message,
@@ -37,7 +37,7 @@ app.get('/', (req, res) => {
     return res.status(200).json({success: true, users: []});
 });
 
-app.use((err, req, res, next) => {
+app.use((err, req, res) => {
     console.error(err.stack);
 
     const errorResponse = {
