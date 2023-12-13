462ce89b8e3db7b91b439252784305f9809975ac
Karacair, Ahmet <kutaykaracair@gmail.com>
2023-12-13 21:09:26 +0300

    Mongodb scripts has been fixed

--- src/scripts/mongodbScripts/createMongodbCollections.js
+++ src/scripts/mongodbScripts/createMongodbCollections.js
@@ -1,21 +1,21 @@
 const  { connectToMongoDB, getMongoDBConnection, closeMongoDB } = require('../../config/mongo.js');
 const {createUserCollection} = require('../../config/mongoSchemas/userSchemaValidation.js');
-const {createResetPasswordCollection} = require('../../config/mongoSchemas/resetPasswordSchemeValidation.js');
+const { createResetPasswordCollection } = require('../../config/mongoSchemas/resetPasswordSchemeValidation.js');
 const Logger = require('../../helpers/logger.js');
 
-Logger.info('Creating MongoDB collections...');
-connectToMongoDB();
+(async () => {
+    try {
+        Logger.info('Creating MongoDB collections...');
+        await connectToMongoDB();
 
-
-try {
-    Logger.info('Collection creation started...');
-    const mongoDBConn = getMongoDBConnection();
-    console.log(mongoDBConn)
-    createUserCollection(mongoDBConn);
-    createResetPasswordCollection(mongoDBConn);
-    Logger.info('Collection creation finished...');
-} catch (error) {
-    Logger.error(`Error creating collections: ${error}`);
-}
-
-closeMongoDB();
+        Logger.info('Collection creation started...');
+        const mongoDBConn = await getMongoDBConnection();
+        await createUserCollection(mongoDBConn);
+        await createResetPasswordCollection(mongoDBConn);
+        Logger.info('Collection creation finished...');
+    } catch (error) {
+        Logger.error(`Error creating collections: ${error}`);
+    } finally {
+        closeMongoDB();
+    }
+})();
