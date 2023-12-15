a6021d33d6b8144fd0ed3a0800d8781d81ab238e
Karacair, Ahmet <kutaykaracair@gmail.com>
2023-12-15 20:21:48 +0300

    Unfinished vacation route

--- src/routes/vacation/createVacation.js
+++ src/routes/vacation/createVacation.js
@@ -1,36 +1,40 @@
-const express = require('express');
-const openai = require('openai');
+const express = require("express");
+const openai = require("openai");
 const router = express.Router();
+
 // POST /vacation
-router.post('/', (req, res) => {
-    // Handle creating a new vacation
-    // You can access request data using req.body
+router.post("/", (req, res) => {
+  const newVacation = {
+    destination: req.body.destination,
+    startDate: req.body.startDate,
+    endDate: req.body.endDate,
+    vacationType: req.body.vacationType,
+    findOtels: req.body.findOtels,
 
-    // Example: Create a new vacation in the database
-    const newVacation = {
-        destination: req.body.destination,
-        startDate: req.body.startDate,
-        endDate: req.body.endDate,
-        // Add more properties as needed
-    };
+    // Add more properties as needed
+  };
 
-    // TODO: Save the new vacation to the database
+  // TODO: Save the new vacation to the database
 
-    // Generate vacation route using ChatGPT
-    const gptResponse = openai.complete({
-        engine: 'text-davinci-003',
-        prompt: `Create a vacation route for ${newVacation.destination}`,
-        maxTokens: 100,
-        temperature: 0.7,
-        n: 1,
-        stop: '\n'
-    });
+  // Generate vacation route using ChatGPT
+  const gptResponse = openai.complete({
+    engine: "text-davinci-003",
+    prompt: `Create a vacation route for ${newVacation.destination}`,
+    maxTokens: 100,
+    temperature: 0.7,
+    n: 1,
+    stop: "\n",
+  });
 
-    // Extract the generated route from the response
-    const generatedRoute = gptResponse.choices[0].text.trim();
+  // Extract the generated route from the response
+  const generatedRoute = gptResponse.choices[0].text.trim();
 
-    // Return a success response with the generated route
-    res.status(201).json({ message: 'Vacation created successfully', vacation: newVacation, route: generatedRoute });
+  // Return a success response with the generated route
+  res.status(201).json({
+    message: "Vacation created successfully",
+    vacation: newVacation,
+    route: generatedRoute,
+  });
 });
 
 module.exports = router;
--- src/routes/vacation/index.js
+++ src/routes/vacation/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const verifyToken = require('../../middlewares/jwtMiddleware');
 const router = express.Router();
+const createVacation = require("./createVacation");
 
 //router.use('/list', getUserList);
-//router.use('/create', verifyToken ,createVacation)
+router.use("/create", verifyToken, createVacation);
 
 
 module.exports = router;
